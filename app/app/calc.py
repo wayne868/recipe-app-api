b76df818444e605f379cefb92ce08fed639c0cbf
wayne868 <jaggernauthw@gmail.com>
2024-04-02 15:19:29 -0400

    upload

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator Functions
+"""
+
+
+def add(x,y):
+    """Add x and y and return result"""
+    return x+y 
+def subtract(x,y):
+    """Subtract x and y and return result"""
+    return y-x
